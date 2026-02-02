093267cf44f1353c34b7a1fd4fdaabbb8c17b75e
mylee99125 <mylee99125@gmail.com>
2026-02-02 23:24:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# React Practice
+
+웹 애플리케이션을 개발모드로 실행
+```bash
+$ npm run dev
+```
+
+이후 [http://localhost:5173](http://localhost:5173)에서 접속 가능
+
+<br>
+
+## Chapter
+Chapter 03. JSX<br>
+Chapter 04. Rendering Elements<br>
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import Library from './chapter_03/Library'
+import Clock from './chapter_04/Clock'
+
+// Chapter 03
+// createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+  //   <Library />
+  // </StrictMode>,
+// )
+
+// Chapter 04
+const root = createRoot(document.getElementById('root'));
+setInterval(() => {
+  root.render(
+    <StrictMode>
+      <Clock />
+    </StrictMode>
+  );
+}, 1000);
